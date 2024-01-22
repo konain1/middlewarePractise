11894b14040150f57210fdf994df2e52121c7658
Konain1 <kaunainkasab@gmail.com>
2024-01-22 12:11:50 +0530

    done

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const zod = require('zod');
 
-const app  = express();
+// app.use(express.json());
+
+function users(req, res, next) {
+    const { username, password, email } = req.headers;
+
+    const userZod = {
+        usernameZod: zod.string().min(2),
+        passwordZod: zod.string().min(8),
+        emailZod: zod.string().email()
+    };
+
+    const result = zod.object(userZod).safeParse({
+        usernameZod: username,
+        passwordZod: password,
+        emailZod: email
+    });
+
+    if(!(username == 'leo' && password == 'leonelmessi' && email == 'leo@gmail.com')){
+        res.json({msg:'username or password or email is wrong!'})
+        return;
+    }
+    if (result.success) { 
+        next();
+    } else {
+        res.json({ msg: 'Error in input data' });
+    }
+}
+
+// app.post('/',users, (req, res) => {
+//     res.send('Zod authentication is successful');
+// });
+
+app.get('/', users,(req, res) => {
+    res.json({ msg: 'Welcome to your login page' });
+});
+
+app.listen(3030, () => {
+    console.log('Server is running on port 3030');
+});
